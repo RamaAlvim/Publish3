1cb47856727a8e01d649b0e46462588c66afabdd
Rama <ranmadmax@gmail.com>
2017-04-04 21:08:42 -0300

    teste
    asd

---
+++ scripts/processElementData.js
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_5IExMRmLEeeUs9rKlNfv8w,_KdXpERmMEeeUs9rKlNfv8wActivityDetail"]="./../../Pluginfinal/deliveryprocesses/resources/DEFINIR MISSÃO DE AVALIAÇÃO_D90709D_d4224efb_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_5IExMRmLEeeUs9rKlNfv8w,_MnTxERmMEeeUs9rKlNfv8wActivityDetail"]="./../../Pluginfinal/deliveryprocesses/resources/TESTAR E AVALIAR_67613171_2df30fcf_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_5IExMRmLEeeUs9rKlNfv8wActivity"]="./../../Pluginfinal/deliveryprocesses/resources/ProcessoTeste_30F44E0A_30f44e0a_Activity.jpeg"
+contentPage.processPage.elementUrls["_Rkp00BmMEeeUs9rKlNfv8w"] = ["analista_de_teste","Pluginfinal/deliveryprocesses/analista_de_teste_35657D8A.html"];
+contentPage.processPage.elementUrls["_KdXpERmMEeeUs9rKlNfv8w"] = ["DEFINIR MISSÃO DE AVALIAÇÃO","Pluginfinal/deliveryprocesses/DEFINIR MISSÃO DE AVALIAÇÃO_D90709D.html"];
+contentPage.processPage.elementUrls["_SeRkcBmMEeeUs9rKlNfv8w"] = ["Definir necessidades de avaliação e rastreabilidade","Pluginfinal/deliveryprocesses/definir_necessidades_de_avaliao_e_rastreabilidade_DAB2EBA9.html"];
+contentPage.processPage.elementUrls["_MnTxERmMEeeUs9rKlNfv8w"] = ["TESTAR E AVALIAR","Pluginfinal/deliveryprocesses/TESTAR E AVALIAR_67613171.html"];
+contentPage.processPage.elementUrls["_TWdJqRmMEeeUs9rKlNfv8w"] = ["Plano de desenvolvimento de software","Pluginfinal/deliveryprocesses/plano_de_desenvolvimento_de_software_41DDEFC9.html"];
+contentPage.processPage.elementUrls["_SeRkcRmMEeeUs9rKlNfv8w"] = ["Plano de desenvolvimento de software","Pluginfinal/deliveryprocesses/plano_de_desenvolvimento_de_software_E550CBB9.html"];
+contentPage.processPage.elementUrls["_UE_qoBmMEeeUs9rKlNfv8w"] = ["Identificar ideias de teste","Pluginfinal/deliveryprocesses/identificar_ideias_de_teste_20AC7794.html"];
+contentPage.processPage.elementUrls["_Rkp00hmMEeeUs9rKlNfv8w"] = ["Plano de testes","Pluginfinal/deliveryprocesses/plano_de_testes_2E9C71B0.html"];
+contentPage.processPage.elementUrls["_TWdJpxmMEeeUs9rKlNfv8w"] = ["Diretrizes de teste (Catálogo de ideias de teste)","Pluginfinal/deliveryprocesses/diretrizes_de_teste_catlogo_de_ideias_de_teste_768301D0.html"];
+contentPage.processPage.elementUrls["_5IExMRmLEeeUs9rKlNfv8w"] = ["ProcessoTeste","Pluginfinal/deliveryprocesses/ProcessoTeste_30F44E0A.html"];
+contentPage.processPage.elementUrls["_TWdJpRmMEeeUs9rKlNfv8w"] = ["Plano de testes","Pluginfinal/deliveryprocesses/plano_de_testes_7D4C0DAA.html"];
+contentPage.processPage.elementUrls["_UokRwBmMEeeUs9rKlNfv8w"] = ["Determinar os resultados dos testes","Pluginfinal/deliveryprocesses/determinar_os_resultados_dos_testes_4A4805CB.html"];
+contentPage.processPage.elementUrls["_SeSLgRmMEeeUs9rKlNfv8w"] = ["Diretrizes de teste (Catálogo de ideias de teste)","Pluginfinal/deliveryprocesses/diretrizes_de_teste_catlogo_de_ideias_de_teste_F7985435.html"];
+contentPage.processPage.elementUrls["_Rkp01BmMEeeUs9rKlNfv8w"] = ["Arquitetura","Pluginfinal/deliveryprocesses/arquitetura_F9F75FA9.html"];
+contentPage.processPage.elementUrls["_TWdJoBmMEeeUs9rKlNfv8w"] = ["Implementar testes","Pluginfinal/deliveryprocesses/implementar_testes_AE1C4B7B.html"];
+contentPage.processPage.elementUrls["_RkpNwBmMEeeUs9rKlNfv8w"] = ["Identificar motivadores de teste","Pluginfinal/deliveryprocesses/identificar_motivadores_de_teste_D3D2A8C1.html"];
+contentPage.processPage.elementUrls["_TWdJoRmMEeeUs9rKlNfv8w"] = ["testador","Pluginfinal/deliveryprocesses/testador_B8BA2B8B.html"];
+contentPage.processPage.elementUrls["_Rkqb4BmMEeeUs9rKlNfv8w"] = ["Requerimento de mudanças","Pluginfinal/deliveryprocesses/requerimento_de_mudanas_5A95F417.html"];
+contentPage.processPage.elementUrls["_Rkp01hmMEeeUs9rKlNfv8w"] = ["Lista de problemas","Pluginfinal/deliveryprocesses/lista_de_problemas_F32E53CF.html"];
+contentPage.processPage.elementUrls["_Rkqb4hmMEeeUs9rKlNfv8w"] = ["Modelo de caso de uso","Pluginfinal/deliveryprocesses/modelo_de_caso_de_uso_53CCE83D.html"];
+contentPage.processPage.elementUrls["_TWdJoxmMEeeUs9rKlNfv8w"] = ["analista_de_teste","Pluginfinal/deliveryprocesses/analista_de_teste_B1F11FB1.html"];
